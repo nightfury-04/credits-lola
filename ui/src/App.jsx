0285cb1e9dadccb7b4b5196f29c9f007cfc05c42
Oswaldo Valadez <mimi_dianita@hotmail.com>
2024-11-25 20:12:42 -0600

    Add missing config

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,47 +1,49 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import axios from 'axios'
+import { useState } from "react";
+import PersonForm from './components/PersonForm';
+import PersonList from "./components/PersonList";
+import Notification from './components/Notification';
 
 function App() {
-  const [data, setData] = useState([]);
+  const [people, setPeople] = useState([]);
+  const [notification, setNotification] = useState(null);
 
-  useEffect(() => {
-    axios
-      .get('https://localhost:7069/api/weatherforecast')
-      .then(response => setData(response.data))
-      .catch(error => console.error('Error:', error));
-  }, []);
+  // Función para mostrar notificaciones
+  const showNotification = (type, message) => {
+    setNotification({ type, message });
+    setTimeout(() => setNotification(null), 3000);
+  }
+
+  // Función para agregar una persona al listado
+  const addPerson = (newPerson) => {
+    setPeople([...people, { ...newPerson, id: people.length + 1}]);
+  }
+
+  // Función para eliminar una persona del listado
+  const deletePerson = (personId) => {
+    setPeople(people.filter((person) => person.id !== personId));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button>
-          count is {data.length}
-        </button>
-        <ul>
-          {data.map((item, index) => (
-            <li key={index}>
-              <strong>Date:</strong> {item.date} |
-              <strong> Temp (C):</strong> {item.temperatureC}°C |
-              <strong> Temp (F):</strong> {item.temperatureF}°F |
-              <strong> Summary:</strong> {item.summary}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
-  )
+  <div className="min-h-screen bg-gray-100 p-4">
+    <h1 className="text-3xl font-bold text-center mb-6">Créditos Doña Lola</h1>
+    {notification && (
+      <Notification 
+        type={notification.type}
+        message={notification.message}
+      />
+    )}
+    <PersonForm 
+      onAddPerson={addPerson}
+      showNotification={showNotification}
+    />
+    <PersonList 
+      people={people} 
+      setPeople={setPeople}
+      onDeletePerson={deletePerson} 
+      showNotification={showNotification}
+    />
+  </div>
+  );
 }
 
-export default App
+export default App;
